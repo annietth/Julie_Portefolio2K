c541a1f837752fc13abc28d3c7af861ab760abca
annie.trn <annietth@users.noreply.github.com>
2020-04-25 19:01:58 +0200

    fix error of menu burger & grid masonry images

--- js/caroussel.js
+++ js/caroussel.js
@@ -1,39 +1,41 @@
-let position = 0;
+// let position = 0;
 
-const $leftArrow = document.querySelector('.left');
-const $rightArrow = document.querySelector('.right');
-const $slides = document.querySelector('.slides'); 
-const sliderLength = document.querySelectorAll('.slide').length; 
-const sliderWidth = document.querySelectorAll('.sliders')
+// const $leftArrow = document.querySelector('.left');
+// const $rightArrow = document.querySelector('.right');
+// const $slides = document.querySelector('.slides'); 
+// const sliderLength = document.querySelectorAll('.slide').length; 
+// const sliderWidth = document.querySelectorAll('.sliders')
 
-$leftArrow.addEventListener('click', left);
-$rightArrow.addEventListener('click', right);
+// $leftArrow.addEventListener('click', left);
+// $rightArrow.addEventListener('click', right);
 
-function left() {
+// function left() {
 
-  if (position == 0) {
-    position = sliderLength - 1; 
-    start(); 
-  } else {
-    position --;
-    move();
-  }
-}
+//   if (position == 0) {
+//     position = sliderLength - 1; 
+//     start(); 
+//   } else {
+//     position --;
+//     move();
+//   }
+// }
 
-function right() {
-  position++;
-  move();
-  if (position == sliderLength - 1) {
-    setTimeout(function() {
-      position = 0;
-      start();
-    }, 300);
-  }
-}
-function move() {
-    $slides.style.transform = 'translateX(' + (position * sliderWidth * -1) + 'px)';
-  }
-function start(){
-    $slides.style.transform='none';
+// function right() {
+//   position++;
+//   move();
+//   if (position == sliderLength - 1) {
+//     setTimeout(function() {
+//       position = 0;
+//       start();
+//     }, 300);
+//   }
+// }
+// function move() {
+//     $slides.style.transform = 'translateX(' + (position * sliderWidth * -1) + 'px)';
+//   }
+// function start(){
+//     $slides.style.transform='none';
 
-}
+// }
+
+
--- js/index.js
+++ js/index.js
@@ -1,26 +1,7 @@
-const navSlide=() => {
-  const burger = document.querySelector('.burger');
-  const nav= document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('nav-links li')
+//menu burger
+const menu = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
 
-
-  burger.addEvenLister('click', ()=>{
-    nav.classList.toggle('nav-active');
-
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation =''
-        
-      } else {
-        llink.style.animation = 'navLinkFade 0.5s ease forwards ${index/7 +2 }s';
-      }
-  
-    });
-
-    burger.classList.toggle('toggle');
-  });
-
-
-}
-
-navSlide()
+menu.addEventListener('click',function () {
+  nav.classList.toggle("nav-active");
+});
