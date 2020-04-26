ce0b8d0e360560c3a706bd58bf89f2d5984999c1
annie.trn <annietth@users.noreply.github.com>
2020-04-26 23:00:10 +0200

    add caroussel js

--- js/caroussel2.js
+++ js/caroussel2.js
@@ -1,43 +1,3 @@
-// let position = 0;
-
-// const $leftArrow = document.querySelector('.left');
-// const $rightArrow = document.querySelector('.right');
-// const $slides = document.querySelector('.slides'); 
-// const sliderLength = document.querySelectorAll('.slide').length; 
-// const sliderWidth = document.querySelectorAll('.sliders')
-
-// $leftArrow.addEventListener('click', left);
-// $rightArrow.addEventListener('click', right);
-
-// function left() {
-
-//   if (position == 0) {
-//     position = sliderLength - 1; 
-//     start(); 
-//   } else {
-//     position --;
-//     move();
-//   }
-// }
-
-// function right() {
-//   position++;
-//   move();
-//   if (position == sliderLength - 1) {
-//     setTimeout(function() {
-//       position = 0;
-//       start();
-//     }, 300);
-//   }
-// }
-// function move() {
-//     $slides.style.transform = 'translateX(' + (position * sliderWidth * -1) + 'px)';
-//   }
-// function start(){
-//     $slides.style.transform='none';
-
-// }
-
 let position = 0;
 
 const $leftArrow = document.querySelector('.slider__arrow--left');
@@ -96,5 +56,3 @@ function jump(callback) {
     });
   });
 }
-
-
