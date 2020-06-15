38999a67403c010851882e74464e7afe2a3b9045
annie.trn <annietth@users.noreply.github.com>
2020-06-16 01:17:56 +0200

    flex

--- js/index.js
+++ js/index.js
@@ -5,3 +5,11 @@ const nav = document.querySelector('.nav-links');
 menu.addEventListener('click',function () {
   nav.classList.toggle("nav-active");
 });
+//masonry
+var container = document.querySelector('#container');
+var containerWidth = container.offsetWidth;
+var msnry = new Masonry( container, {
+  // options
+  columnWidth: containerWidth / 3,
+  itemSelector: '.flex-item'
+});
